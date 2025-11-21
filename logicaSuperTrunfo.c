d931d31fb81c8691daf4f1c196fac053183178c1
kkeletronica <kkeletronica@gmail.com>
2025-11-21 03:03:22 -0300

    implementada funcao esclusiva para calculos

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -12,35 +12,9 @@ struct Carta
   pibPerCapta, superPoder;
   int pontos_turisticos; 
   unsigned long int populacao;
+  
 
 };
-
-
-/*
-  função para calcular densidade populacional:
-  Recebe o float cx_populacao e float cx_area > calcula densidade (densidade = população / área ocupada km²)
-  > Retorna var com o valor do cálculo da densidade populacional em hab/km²
-*/ 
-float densidadePopulacional(unsigned long int populacao, float area)
-{
-
-  float densidadePop = populacao / area;
-
-  return densidadePop;
-}
-
-/*
-  Função para calcular o PIB per capta:
-  Recebe float cx_pib e int cx_populacao > calcula: Pib / População e armazena em uma var > Retorna var com cálculo para main.
-*/
-float pibPerCapta(float pib, unsigned long int populacao)
-{
-  float pibPerCapta = pib / populacao;
-
-  return pibPerCapta;
-}
-
-
 
 
 
@@ -51,56 +25,57 @@ float pibPerCapta(float pib, unsigned long int populacao)
   int pontosTuristicos;
   unsigned long int populacao. 
 */
-void exibeCartas(
-  int carta,
-  char estado, char *codigo, char *cidade,
-  float area, float pib, float densidadePopulacional, float pibPerCapta,
-  int pontosTuristicos, unsigned long int populacao, float superPoder
-)
+void exibeCartas(int carta, struct Carta *c)
 { 
 
   printf("\t= CARTA #%d =\n", carta);
-  printf("\tEstado: %c\n", estado);
-  printf("\tCodigo: %s\n", codigo);
-  printf("\tCidade: %s\n", cidade);
-  printf("\tPopulacao: %ld\n", populacao);
-  printf("\tArea: %.2f Km^2\n", area);
-  printf("\tPIB: %.2f\n", pib);
-  printf("\tPontos Turisticos: %d\n", pontosTuristicos);
-  printf("\tDensidade populacional: %.2f HAB/Km^2\n", densidadePopulacional);
-  printf("\tPIB per capta: R$ %.2f\n", pibPerCapta);
-  printf("\tSUPER PODER: %.2f bi\n\n", (superPoder / 1e9));
+  printf("\tEstado: %c\n", c->estado);
+  printf("\tCodigo: %s\n", c->codigo);
+  printf("\tCidade: %s\n", c->cidade);
+  printf("\tPopulacao: %ld\n", c->populacao);
+  printf("\tArea: %.2f Km^2\n", c->area);
+  printf("\tPIB: %.2f\n", c->pib);
+  printf("\tPontos Turisticos: %d\n", c->pontos_turisticos);
+  printf("\tDensidade populacional: %.2f HAB/Km^2\n", c->densidadePopulacional);
+  printf("\tPIB per capta: R$ %.2f\n", c->pibPerCapta);
+  printf("\tSUPER PODER: %.2f bi\n\n", (c->superPoder / 1e9));
 
 
 
 }
 
 /*
-  O SUPER PODER é calculado somando 
-  população, área, PIB, número de pontos turísticos, PIB per capita e o inverso da densidade populacional 
+  funcao realiza calculos Densidade Populacional
+  PIB per capta e Super poder
 */
-float superPoder(
-  float area, float pib, float densidadePopulacional, float pibPerCapta,
-  float pontosTuristicos, float populacao
-)
+void calculos(struct Carta *c)
 {
   
-  float super = (populacao + area + pib + pontosTuristicos + pibPerCapta) + (-densidadePopulacional);
+    c->superPoder = (c->populacao + c->area + c->pib + c->pontos_turisticos + c->pibPerCapta) + (-c->densidadePopulacional);
+    c->densidadePopulacional = c->populacao / c->area;
+    c->pibPerCapta = c->pib / c->populacao;
 
-  return super;
+    printf("%f", c->superPoder);
 
 }
 
 
 
+
 //COMPARADOR DE CARTAS, DETERMINA O VENCEDOR.
-void batalha(struct Carta *c1, struct Carta *c2)
+void menuBatalha(struct Carta *c1, struct Carta *c2)
 {
-    int opt1;
-    int opt2;
+        
     char atributo[50];
     char atributo2[50];
-
+    
+    int opt1, opt2;
+    
+    float carta1 = 0;
+    float carta1_2 = 0;
+    float carta2 = 0;
+    float carta2_2 = 0;
+    
     printf("\n\t\t==== SUPER TRUNFO - BATALHA DE CARTAS ====\n\n");
     printf("\tEscolha DUAS OPCOES de atributo que sera usado para essa jogada:\n");
     printf("\t1 - Populacao\n");
@@ -113,17 +88,9 @@ void batalha(struct Carta *c1, struct Carta *c2)
     
     printf("\t-> Digite o numero da PRIMEIRA opcao desejada: ");
     scanf("%d", &opt1);
-    printf("\t-> Digite o numero da SEGUNDA opcao desejada (diferente do primeiro): ");
+    printf("\t-> Digite o numero da SEGUNDA opcao desejada: ");
     scanf("%d", &opt2);
-
-    //validado a opção, se o valores escolhidos nao forem iguais, maior que zero e menor que 7 = valido.
-    // ((opt1 > 0 && opt2 > 0) && (opt1 < 8 && opt2 < 8) && (opt1 != opt2)) ? (validacao = 1): (validacao = 0);
     
-    
-    float carta1 = 0;
-    float carta1_2 = 0;
-    float carta2 = 0;
-    float carta2_2 = 0;
     
     switch(opt1)
     {
@@ -132,37 +99,37 @@ void batalha(struct Carta *c1, struct Carta *c2)
             carta1 = c1->populacao;
             carta2 = c2->populacao;
             break;
-
+    
         case 2:
             strcpy(atributo, "Area");
             carta1 = c1->area;
             carta2 = c2->area;
             break;
-
+    
         case 3:
             strcpy(atributo, "PIB");
             carta1 = c1->pib;
             carta2 = c2->pib;
             break;
-
+    
         case 4:
             strcpy(atributo, "Pontos Turisticos");
             carta1 = c1->pontos_turisticos;
             carta2 = c2->pontos_turisticos;
             break;
-
+    
         case 5:
             strcpy(atributo, "Densidade Populacional");
             carta1 = c1->densidadePopulacional;
             carta2 = c2->densidadePopulacional;
             break;
-
+    
         case 6:
             strcpy(atributo, "PIB per Capta");
             carta1 = c1->pibPerCapta;
             carta2 = c2->pibPerCapta;
             break;
-
+    
         case 7:
             strcpy(atributo, "Super Poder");
             carta1 = c1->superPoder;
@@ -170,56 +137,72 @@ void batalha(struct Carta *c1, struct Carta *c2)
             break;
         default:
             printf("\t\t xxx Opcao Invalida xxx\n");
+            
             break;
     }
+
 
     switch(opt2)
     {
         case 1:
             strcpy(atributo2, "Populacao");
-            carta1_2 = c1->populacao;
-            carta2_2 = c2->populacao;
+            (opt1 == opt2)? (printf("As opcoes precisam ser diferentes.")): 
+                            (carta1_2 = carta2_2 = c1->populacao = c2->populacao);
             break;
-
+    
         case 2:
             strcpy(atributo2, "Area");
-            carta1_2 = c1->area;
-            carta2_2 = c2->area;
+            (opt1 == opt2)? (printf("As opcoes precisam ser diferentes.")): 
+                            (carta1_2 = carta2_2 = c1->area = c2->area);
+            // carta1 = c1->area;
+            // carta2 = c2->area;
             break;
-
+    
         case 3:
             strcpy(atributo2, "PIB");
-            carta1_2 = c1->pib;
-            carta2_2 = c2->pib;
+            (opt1 == opt2)? (printf("As opcoes precisam ser diferentes.")): 
+                            (carta1_2 = carta2_2 = c1->pib = c2->pib);
+            // carta1 = c1->pib;
+            // carta2 = c2->pib;
             break;
-
+    
         case 4:
             strcpy(atributo2, "Pontos Turisticos");
-            carta1_2 = c1->pontos_turisticos;
-            carta2_2 = c2->pontos_turisticos;
+            (opt1 == opt2)? (printf("As opcoes precisam ser diferentes.")): 
+                            (carta1_2 = carta2_2 = c1->pontos_turisticos = c2->pontos_turisticos);
+            // carta1 = c1->pontos_turisticos;
+            // carta2 = c2->pontos_turisticos;
             break;
-
+    
         case 5:
             strcpy(atributo2, "Densidade Populacional");
-            carta1_2 = c1->densidadePopulacional;
-            carta2_2 = c2->densidadePopulacional;
+            (opt1 == opt2)? (printf("As opcoes precisam ser diferentes.")): 
+                            (carta1_2 = carta2_2 = c1->densidadePopulacional = c2->densidadePopulacional);
+            // carta1 = c1->densidadePopulacional;
+            // carta2 = c2->densidadePopulacional;
             break;
-
+    
         case 6:
             strcpy(atributo2, "PIB per Capta");
-            carta1_2 = c1->pibPerCapta;
-            carta2_2 = c2->pibPerCapta;
+            (opt1 == opt2)? (printf("As opcoes precisam ser diferentes.")): 
+                            (carta1_2 = carta2_2 = c1->pibPerCapta = c2->pibPerCapta);
+            // carta1 = c1->pibPerCapta;
+            // carta2 = c2->pibPerCapta;
             break;
-
+    
         case 7:
             strcpy(atributo2, "Super Poder");
-            carta1_2 = c1->superPoder;
-            carta2_2 = c2->superPoder;
+            (opt1 == opt2)? (printf("As opcoes precisam ser diferentes.")): 
+                            (carta1_2 = carta2_2 = c1->superPoder = c2->superPoder);
+            // carta1 = c1->superPoder;
+            // carta2 = c2->superPoder;
             break;
         default:
             printf("\t\t xxx Opcao Invalida xxx\n");
+            
             break;
     }
+   
 
     printf("\n\n\tCARTA #1 - %s - %s: %.2f\n",c1->cidade, atributo, carta1);
     printf("\tCARTA #1 - %s - %s: %.2f\n",c1->cidade, atributo2, carta1);
@@ -229,13 +212,18 @@ void batalha(struct Carta *c1, struct Carta *c2)
     printf("\tCARTA #2 - %s - %s: %.2f\n",c2->cidade, atributo2, carta2);
     printf("\tCARTA #2 - %s - TOTAL: %.2f\n\n",c2->cidade, (carta2 + carta2_2));
 
-    ((carta1 + carta1_2) > (carta2 + carta2_2))?
-        printf("\t^^^^ !! VITORIA DA CARTA #1 !! ^^^^\n"):
+    
+    if((carta1 + carta1_2) > (carta2 + carta2_2)){
+        
+       printf("\t^^^^ !! VITORIA DA CARTA #1 !! ^^^^\n");
+    }else if((carta1 + carta1_2) == (carta2 + carta2_2)){
+        printf("\t^^^^ !! EMPATOU !! ^^^^\n");
+        
+    }else if(carta1){
+
+    }else{
         printf("\t^^^^ !! VITORIA DA CARTA #2 !! ^^^^\n");
 
-    if (carta1 == carta2){
-
-        printf("\t^^^^ !! EMPATOU !! ^^^^\n");
     }
 
         
@@ -250,9 +238,9 @@ void batalha(struct Carta *c1, struct Carta *c2)
 */
 void cadastraCartas(struct Carta *c)
 {
-  printf("\n\t\t==== CADASTRANDO CARTAS ====\n"); 
+  printf("\n\t\t==== CADASTRANDO NOVA CARTA ====\n"); 
   
-  printf("Por favor, digite uma letra para o estado de A a H (ex: A): ");
+  printf("Por favor, digite uma letra para o estado de A a H: ");
   scanf("%c", &c->estado);
   scanf("%*[^\n]"); scanf("%*c"); //LIMPA O BUFFER
 
@@ -267,7 +255,7 @@ void cadastraCartas(struct Carta *c)
   printf("Digite a populacao da cidade (ex: 92060): ");
   scanf("%ld", &c->populacao);
 
-  printf("Digite a area da cidade em Km^2 (ex: 307.553): ");
+  printf("Digite a area da cidade em Km^2 (ex: 307.55): ");
   scanf("%e", &c->area);
 
   printf("Digite o PIB da cidade (ex: 3108224143.00): ");
@@ -277,56 +265,27 @@ void cadastraCartas(struct Carta *c)
   scanf("%d", &c->pontos_turisticos);
   scanf("%*[^\n]"); scanf("%*c"); //LIMPA O BUFFER
   
+  
+  
 }
 
 int main() {
   
-  struct Carta c1;//carta1
-  struct Carta c2; //carta2
-  
+    struct Carta c1;//carta1
+    struct Carta c2; //carta2
   // INICIANDO CADASTRO DE CARTAS
-  cadastraCartas(&c1);
-  cadastraCartas(&c2);
-  
-/*
-  Enviando dados coletados para funções de cálculo de densidade populacional e pib per capta
-*/
+    cadastraCartas(&c1);
+    // cadastraCartas(&c2);
+    
+    calculos(&c1);
+    printf("\ndentro da main %f\n", c1.superPoder);
+    // calculos(&c2);
+ 
+    exibeCartas(1, &c1);
+    // exibeCartas(2, &c2 );
 
-  c1.densidadePopulacional = densidadePopulacional(c1.populacao, c1.area);
-  c1.pibPerCapta = pibPerCapta(c1.pib, c1.populacao); 
-  c1.superPoder = superPoder(c1.area, c1.pib, c1.densidadePopulacional,
-                             c1.pibPerCapta, c1.pontos_turisticos, 
-                             c1.populacao
-                            );
-  
-  c2.densidadePopulacional = densidadePopulacional(c2.populacao, c2.area);
-  c2.pibPerCapta = pibPerCapta(c2.pib, c2.populacao); 
-  c2.superPoder = superPoder(c2.area, c2.pib, c2.densidadePopulacional,
-                             c2.pibPerCapta, c2.pontos_turisticos, 
-                             c2.populacao
-                            ); 
+    // menuBatalha(&c1, &c2);
 
-
-  // Área para exibição dos dados da cidade
-  //Passando as variáveis para a função para exibição das cartas
-  
-  exibeCartas(
-    1,
-    c1.estado, c1.codigo, c1.cidade, c1.area, c1.pib,
-    c1.densidadePopulacional, c1.pibPerCapta,
-    c1.pontos_turisticos, c1.populacao,
-    c1.superPoder
-  );
-
-  exibeCartas(
-    2,
-    c2.estado, c2.codigo, c2.cidade, c2.area, c2.pib,
-    c2.densidadePopulacional, c2.pibPerCapta,
-    c2.pontos_turisticos, c2.populacao,
-    c2.superPoder
-  );
-
-  batalha(&c1, &c2);
   
 return 0;
 } 
